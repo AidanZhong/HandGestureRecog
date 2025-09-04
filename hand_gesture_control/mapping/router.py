235f13f5153cf3d988e87cc377c091271480c99d
50601 <506017129@qq.com>
2025-09-04 22:31:22 +0100

    implemented rules.py

--- hand_gesture_control/mapping/router.py
+++ hand_gesture_control/mapping/router.py
@@ -8,3 +8,23 @@ Created on 2025/9/4 12:29
 @description: Dispatches GestureEvent to controllers based on mapping rules
 - Python 
 """
+from hand_gesture_control.core.state import History, ModeState
+from hand_gesture_control.core.types import GestureEvent
+
+
+class GestureRouter:
+    def __init__(self, engine, cfg, debouncer):
+        self.engine = engine
+        self.cfg = cfg
+        self.debouncer = debouncer
+
+    def handle(self, event: GestureEvent, hist: History, modes: ModeState):
+        '''
+            Orchestrate order of operations per frame
+            1. check sequences
+            2. handle release
+            3. run continuous controller based on gestures
+            4. handle mode exits
+            5. update history
+        '''
+        # todo
