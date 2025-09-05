5e63ac08609a9aa99550ad6205bbf7a07e51bb90
50601 <506017129@qq.com>
2025-09-05 09:48:58 +0100

    implemented os drivers

--- hand_gesture_control/actions/controllers/pointer.py
+++ hand_gesture_control/actions/controllers/pointer.py
@@ -8,3 +8,14 @@ Created on 2025/9/4 12:37
 @description: Move/drag/scroll logic
 - Python 
 """
+from hand_gesture_control.actions.os_driver import OSDriver
+
+
+class PointerController:
+    def __init__(self, driver:OSDriver, width, height, ema, deadzone_eps, move_gain):
+        self.driver = driver
+        self.width = width
+        self.height = height
+        self.ema = ema
+        self.deadzone_eps = deadzone_eps
+        self.move_gain = move_gain
